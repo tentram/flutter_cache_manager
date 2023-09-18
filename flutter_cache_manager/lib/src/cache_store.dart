f49d293829dc8b9100851d9fc53464aba568bda8
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-09-18 19:09:46 +0700

    export cache_object (PR by @tristan-vrt: https://github.com/Baseflow/flutter_cache_manager/pull/371)

--- flutter_cache_manager/lib/src/cache_store.dart
+++ flutter_cache_manager/lib/src/cache_store.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-import 'package:flutter_cache_manager/src/storage/cache_object.dart';
 import 'package:flutter_cache_manager/src/storage/file_system/file_system.dart';
 
 ///Flutter Cache Manager
